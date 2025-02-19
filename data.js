6d1a5cb1c77e3a83615312b876bfa78b39eb2629
rajnish69 <rajnishrabha7@gmail.com>
2025-02-19 12:44:44 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hall",
+      "name": "HALL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.014046654063411879,
+        "pitch": 0.04468037380546974,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03402532537397995,
+          "pitch": 0.018036063686880155,
+          "rotation": 0,
+          "target": "1-hall2"
+        },
+        {
+          "yaw": -2.1171138914404573,
+          "pitch": -0.028062247614972335,
+          "rotation": 0,
+          "target": "3-entertainment-room"
+        },
+        {
+          "yaw": -0.6426913646882149,
+          "pitch": -0.06928730862240329,
+          "rotation": 4.71238898038469,
+          "target": "4-guest-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hall2",
+      "name": "HALL2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.031339921682796756,
+          "pitch": 0.00284740498078051,
+          "rotation": 4.71238898038469,
+          "target": "2-daughter-room"
+        },
+        {
+          "yaw": 0.9075351108265526,
+          "pitch": 0.012123067774776786,
+          "rotation": 1.5707963267948966,
+          "target": "6-sons-room-2"
+        },
+        {
+          "yaw": 3.1332652809038812,
+          "pitch": 0.033417426106964854,
+          "rotation": 0,
+          "target": "4-guest-room"
+        },
+        {
+          "yaw": -1.5758903231811097,
+          "pitch": -0.013216533133565633,
+          "rotation": 0,
+          "target": "5-sons-room-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-daughter-room",
+      "name": "DAUGHTER ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5955356438978043,
+          "pitch": -0.0013538607673737602,
+          "rotation": 0,
+          "target": "1-hall2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-entertainment-room",
+      "name": "ENTERTAINMENT ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7524878002906306,
+          "pitch": -0.010361875123862774,
+          "rotation": 0,
+          "target": "0-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-guest-room",
+      "name": "GUEST ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.038152948837972,
+          "pitch": -0.04282401074130604,
+          "rotation": 0,
+          "target": "1-hall2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-sons-room-1",
+      "name": "SONS ROOM 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8476892020083042,
+          "pitch": 0.009641373380656404,
+          "rotation": 0,
+          "target": "1-hall2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sons-room-2",
+      "name": "SONS ROOM 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7327110772992089,
+          "pitch": 0.027401179367540607,
+          "rotation": 0,
+          "target": "1-hall2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "DAWE VILLA 360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
